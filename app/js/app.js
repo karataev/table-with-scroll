e91a9d507c7f40bf4405c616852a3d805e3fc8d9
karataev <eugene.karataev@gmail.com>
2016-01-28 00:42:18 +0600

    Move data to JSON

--- app/js/app.js
+++ app/js/app.js
@@ -1,37 +1,27 @@
 
 angular.module('app', [])
 
-  .controller('AppCtrl', [function () {
+  .controller('AppCtrl', ['$http', function ($http) {
     var vm = this;
-
-    vm.blankData = [
-      {
-        value: 'Строка 1'
-      },
-      {
-        value: 'Строка 2'
-      },
-      {
-        value: 'Строка 3'
-      }
-    ]
-
-    vm.data = [
-      {
-        value: 'Данные 1'
-      },
-      {
-        value: 'Данные 2'
-      },
-      {
-        value: 'Данные 3'
-      }
-    ]
 
     vm.expanded = false;
 
     vm.toggleExpanded = function () {
       vm.expanded = !vm.expanded;
+    };
+
+    function fetchData() {
+      $http.get('data/blank.json')
+        .then(function (response) {
+          vm.blankData = response.data;
+        });
+
+      $http.get('data/data.json')
+        .then(function (response) {
+          vm.data = response.data;
+        });
     }
+
+    fetchData();
 
   }])
